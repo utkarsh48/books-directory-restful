cf1a8bc642def7f321ef8b81ee9bd970d0ac5069
Utkarsh Dhiman <47657465+utkarsh48@users.noreply.github.com>
2021-12-15 09:37:36 +0000

    add: link back to documentation

--- startup/routes.js
+++ startup/routes.js
@@ -7,4 +7,8 @@ module.exports = function(app){
 	app.use(express.json());
 	app.use("/api/books", books);
 	app.use("/api/user", user);
+
+	app.get("/", (req, res)=>{
+		res.send("Refer to documentation <a href='https://github.com/utkarsh48/books-directory-restful#readme'>here</a>");
+	})
 }
